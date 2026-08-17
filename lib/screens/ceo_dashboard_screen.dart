c097b5a88e54849df4228b8da3314e529ff04c98
Agwince Kagali <agwincekagali@gmail.com>
2026-08-17 23:09:27 +0300

    fix: add missing supabase import

--- lib/screens/ceo_dashboard_screen.dart
+++ lib/screens/ceo_dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../services/supabase_service.dart';
 import '../services/auth_service.dart';
 
